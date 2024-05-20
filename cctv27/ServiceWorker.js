6571a8e9014aa9dd91381975a78215a94bb6a51e
Rob Schellingerhout <113621039+RobSchellingerhout@users.noreply.github.com>
2024-05-20 10:19:00 +0200

    Add files via upload

---
+++ cctv27/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "RSDV-cctv-0.1";
+const contentToCache = [
+    "Build/cctv27.loader.js",
+    "Build/cctv27.framework.js.unityweb",
+    "Build/cctv27.data.unityweb",
+    "Build/cctv27.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
